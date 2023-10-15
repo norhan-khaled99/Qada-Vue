c6dceda047021d527172ba4f87deeabac0a40b4a
Nora Mohammed <NoorMohammed3399@gmail.com>
2023-10-15 18:13:38 +0300

    add nav and footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,30 +1,50 @@
 <template>
-    <div class="hello">
-         <h1>footer</h1>
+  <footer class="text-light text-center py-3">
+    <div class="container">
+      <h1>قدة</h1>
+      <div class="row">
+        <div class="col-lg-1 offset-lg-3">
+          <p>إتصل بنا</p>
+        </div>
+        <div class="d-flex justify-content-center col-8">
+          <div class="mx-3">
+            <p>الاسئلة المتكررة</p>
+          </div>
+          <div class="mx-3">
+            <p>من نحن؟</p>
+          </div>
+          <div class="mx-3">
+            <p>الرئيسية</p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <p>الشروط والاحكام</p>
+      </div>
+      <div class="row ">
+        <p class="w-75">
+          Lörem ipsum od ohet dilogi. Bell trabel, samuligt, ohöbel utom diska.
+          Jinesade bel när feras redorade i belogi. FAR paratyp i muvåning, och
+          pesask vyfisat. Viktiga poddradio har un mad och inde.
+        </p>
+      </div>
     </div>
-  </template>
+  </footer>
+</template>
   
   <script>
-  export default {
-
-  }
-  </script>
+export default {};
+</script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
-  }
-  </style>
+@import url("https://fonts.googleapis.com/css2?family=Cairo&display=swap");
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background-color: #0c483f;
+  font-family: "Cairo", sans-serif;
+}
+</style>
   
