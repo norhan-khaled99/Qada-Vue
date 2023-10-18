6ecfbdedf2be5cfff5cf6a36090c2e6d41efe521
norhan-khaled99 <norhan-khaled99@github.com>
2023-10-18 14:37:21 +0300

    finish projects phases ❤

--- src/modules/members/pages/Tabs/projectPhases.vue
+++ src/modules/members/pages/Tabs/projectPhases.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="my-5">
-    <div class="card col-md-6 col-sm-12 text-center">
+    <div class="card col-md-7 col-sm-12 text-center">
         <div class="card-body">
              <span class="card-content">تنويه</span>: حفاظًا علي الجدول الزمني لمشروعك يلزم إرسال طلب بدء أعمال للمكتب الهندسي.
         </div>
@@ -12,17 +12,26 @@
     </div>
 
     <div class="card col-md-6 col-sm-12"> 
-      <div class="d-flex  justify-content-between  seperator ">
-        <span>المراحل المستلمة</span>
-        <span><button class="btn first-stage">طلب تعديل</button></span>
-        <span><button class="btn recieve-stage">استلام المرحلة</button></span>
+      <div class="d-flex justify-content-between align-items-baseline ms-3 me-3 my-1">
+          <div class="card-title text-right">المراحل المستلمة</div>
+          <div class="">
+              <button class="btn first-stage">طلب تعديل</button>
+              <button class="btn recieve-stage">استلام المرحلة</button>
+          </div>
       </div>
-      <div class="row d-flex justify-content-between align-items-center  seperator">
-        <span>المراحل المستلمة</span>
-        <span><button class="btn first-stage">طلب تعديل</button></span>
-        <span><button class="btn recieve-stage">استلام المرحلة</button></span>
-      </div>
+      <div class="seperator"></div>
+      <div class="d-flex justify-content-between align-items-baseline ms-3 me-3 my-1">
+        <div class="text-right">
+          <span>عدد ايام المرحلة: <span class="numbers">  (10) يوم | </span></span>
+          <span>عدد التعديلات: <span class="numbers">2/5</span></span>        
+        </div>
+        <div class="button-stage">
+          <button class="btn request-start-working ">طلب بدءالاعمال</button>
+            <button class="btn edit-button ">توقيع غرامة تأخير</button>
+        </div>
     </div>
+  </div>
+  
     </div>
   </div>
 </template>
@@ -42,14 +51,29 @@ margin: auto;
 border-radius: 10px;
 border: 1px solid rgba(0, 0, 0, 0.70);
 }
+.card-body{
+color: #000;
+font-family: Inter;
+font-size: 24px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+}
+.card-title{
+color: #336059;
+text-align: right;
+font-family: Cairo;
+font-size: 17px;
+font-weight: 600;
+}
 .card-content{
-color: #0C483F;
+color: #259F5A;
 text-align: center;
 font-size: 24px;
 font-weight: 700;
 }
 .header{
-color: #0C483F;
+color: #259F5A;
 font-family: Cairo;
 font-size: 25px;
 font-weight: 700;
@@ -58,15 +82,42 @@ text-align: center;
 }
 .first-stage{
 border-radius: 5px;
-border: 2px solid #48847B;
+border: 2px solid hsl(171, 29%, 40%);
 color: #48847B;
+margin-left: 0.5rem;
+padding: 5px 30px;
+
 }
 .recieve-stage{
 border-radius: 5px;
 background: #48847B;
+color:#FFFFFF;
+padding: 5px 25px;
 }
 .seperator{
-border-bottom: 2px solid #48847B;
+border-bottom: 1px solid #48847B;
+width: 99.9%;
+}
+.button-stage button{
+  margin: 0rem 0.25rem;
+}
+.numbers{
+color: #259F5A;
+font-family: Cairo;
+font-size: 17px;
+font-weight: 700;
+}
+.edit-button{
+border-radius: 5px;
+background: #CF2525;
+color:#FFFFFF;
+padding: 5px 15px;
+}
+.request-start-working{
+  border-radius: 5px;
+  background: #48847B;
+  color:#FFFFFF;
+  padding: 5px 15px;
 }
 
 </style>
---
+++ src/modules/members/pages/Tabs/projectTransactions.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="table-container">
+      <table class="responsive-table">
+        <tr>
+          <th>Column 1</th>
+          <th>Column 2</th>
+          <th>Column 3</th>
+          <th>Column 4</th>
+          <th>Column 5</th>
+        </tr>
+        <tr>
+          <td>Data 1</td>
+          <td>Data 2</td>
+          <td>Data 3</td>
+          <td>Data 4</td>
+          <td>Data 5</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.table-container {
+  overflow-x: auto;
+}
+
+.responsive-table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.responsive-table th,
+.responsive-table td {
+border: 1px solid #0c483f;
+  padding: 10px;
+  text-align: center;
+  border-radius: 10px; 
+}
+</style>
