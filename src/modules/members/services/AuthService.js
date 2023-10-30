78e6b1af18a6eb7451385963e34eac49f7f7e6c4
nadahamed <nadahamed123455@gmail.com>
2023-10-30 20:54:27 +0200

    ...

---
+++ src/modules/members/services/AuthService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+const apiClient = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+  headers: {},
+});
+
+const authService = {
+  personData: {},
+  register(credentials) {
+    return apiClient.post("person/register", credentials);
+  },
+  login(credentials) {
+    return apiClient.post("person/login", credentials);
+  },
+};
+
+export default authService;
+
+
